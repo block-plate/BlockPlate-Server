702a97e12c572ab2b4c1f52bcb98ffe6d6753288
yunseo323 <jokani3244833@gmail.com>
2023-05-15 17:44:06 +0900

    userCreateEX 수정

--- src/user/mock/index.ts
+++ src/user/mock/index.ts
@@ -2,6 +2,7 @@ import { UserCreateInputDTO } from '../dto/create_user.dto';
 
 export const userCreateInputEX: UserCreateInputDTO = {
   email: 'user@gmail.com',
+  account: 'b351d83937dcf6a1407857b446d1d63208d79049',
   pwd: '4833',
   name: 'user1',
   type: 0,
